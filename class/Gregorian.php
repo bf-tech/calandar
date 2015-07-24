3a6ddbde5384679231a4219e11f3ed961bfe7cee
bf-tech <fethi.bendimerad@icloud.com>
2015-07-24 13:08:36 -0400

    working test page of total number of days until a given date

--- class/Gregorian.php
+++ class/Gregorian.php
@@ -2,34 +2,50 @@
 
 class Gregorian {
 
-	// Something to say
-
-	public $day = 0;
-	public $month = 0;
-	public $year = 0;
+	public $gregorianDate = '';
+	public $gregorianDays = 0;
 
 	function __construct() {
 		date_default_timezone_set('America/New_York');
 		echo 'Gregorian Class constructed at '.date("H:i:s").'<br/>';
+
+		/*
+			Initiate the gregorianDate to the current date in case none is provided.
+		*/
+		$this->gregorianDate = date('Y-m-d');
 	}
 
-	public function inDays() {
-
+	public function setGivenDate($d, $m, $y) {
+		return $this->gregorianDate = date('Y-m-d', mktime(0, 0, 0, $m, $d, $y));
 	}
 
-	public function fromFirst() {
+	public function gregorianDate() {
+
+		return date('l d F Y L',strtotime($this->gregorianDate));
+	}
+
+	public function gregorianDays() {
 
 		$firstMoharram = new DateTime('622-7-16');
+		$givenDate = new DateTime($this->gregorianDate);
 		$thursdayOctobre = new DateTime('1582-10-4');
-		$fridayOctobre = new DateTime('1582-10-15');
-		// $unixStart = new DateTime('1970-1-1');
-		$current = date('Y-m-d');
-		$currentDate = new DateTime($current);
 
-		return $firstMoharram->diff($thursdayOctobre)->days + $fridayOctobre->diff($currentDate)->days;
+		if ($givenDate <= $thursdayOctobre) {
+
+			return $this->gregorianDays = $firstMoharram->diff($givenDate)->days;
+
+		} else {
+
+			$fridayOctobre = new DateTime('1582-10-15');
+		
+			return $this->gregorianDays = $firstMoharram->diff($thursdayOctobre)->days + $fridayOctobre->diff($givenDate)->days; //same as having total minus 10 days
+		}
 	}
 
-	public function test() {
+	/*
+		This function show that the DateTime::diff doesn't take in consideration de 10 days jump of October 1582 by returning the value of 11 instead of 1.
+	*/
+	public function thursdayFriday() {
 		$firstDate = new DateTime('1582-10-4');
 		$lastDate = new DateTime('1582-10-15');
 
--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php
 
-include_once('helper/autoload.php');
+if (isset($_GET['year'])) {
 
-$Greg = new Gregorian;
+	include_once('helper/autoload.php');
 
-echo $Greg->fromFirst();
+	$Greg = new Gregorian;
+
+	echo $Greg->gregorianDays().' days from 1 Moharram year I Hijir til today<br/><br/>';
 
 
-?>
+	echo $Greg->setGivenDate($_GET['day'], $_GET['month'], $_GET['year']).' Given Date<br/>';
+
+	echo $Greg->gregorianDate().'<br/>';
+
+	echo $Greg->gregorianDays().' days from 1 Moharram I Hijir til the Given Date<br/>';
+
+} else { ?>
+	
+	<form method="get" action="<?php $_SERVER['PHP_SELF'] ?>">
+		<input id ="d" type="number" name="day" min="1" max="31">
+		<input id ="m" type="number" name="month" min="1" max="12">
+		<input id ="y" type="number" name="year" min="622" max="2500">
+		<input type="submit">
+	</form>
+
+<?php } ?>
